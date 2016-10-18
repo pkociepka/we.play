64fe6b08163b997848be0cbf1be3fb9b13b75c42
Kurtzz <przemyslaw.kurc@gmail.com>
2016-10-18 12:35:27 +0200

    Add AuthoritiesConstants

---
+++ src/main/java/pl/edu/agh/weplay/security/AuthoritiesConstants.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.weplay.security;
+
+/**
+ * Created by P on 18.10.2016.
+ */
+public final class AuthoritiesConstants {
+
+    public static final String ADMIN = "ROLE_ADMIN";
+
+    public static final String USER = "ROLE_USER";
+
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+
+    private AuthoritiesConstants() {
+    }
+}
