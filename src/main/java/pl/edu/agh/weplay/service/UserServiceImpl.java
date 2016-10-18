a6f09f65852c894253c936ed5b5be0bb80a4696c
Kurtzz <przemyslaw.kurc@gmail.com>
2016-10-18 12:22:21 +0200

    Add createUser method

--- src/main/java/pl/edu/agh/weplay/service/UserServiceImpl.java
+++ src/main/java/pl/edu/agh/weplay/service/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package pl.edu.agh.weplay.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.edu.agh.weplay.domain.Authority;
 import pl.edu.agh.weplay.domain.User;
 import pl.edu.agh.weplay.repository.AuthorityRepository;
 import pl.edu.agh.weplay.repository.UserRepository;
+import pl.edu.agh.weplay.service.util.RandomUtil;
+import pl.edu.agh.weplay.web.rest.dto.UserDTO;
 
 import javax.inject.Inject;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by P on 18.10.2016.
@@ -20,6 +26,28 @@ public class UserServiceImpl implements UserService {
 
     @Inject
     AuthorityRepository authorityRepository;
+
+    @Inject
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User createUser(UserDTO userDTO) {
+        User user = new User();
+        user.setLogin(userDTO.getLogin());
+
+        if (userDTO.getAuthorities() != null) {
+            Set<Authority> authorities = new HashSet<>();
+            userDTO.getAuthorities().stream().forEach(
+                    authority -> authorities.add(authorityRepository.findOne(authority))
+            );
+            user.setAuthorities(authorities);
+        }
+        String encryptedPassword = passwordEncoder.encode(RandomUtil.generatePassword());
+        user.setPassword(encryptedPassword);
+        userRepository.save(user);
+
+        return user;
+    }
 
     public User getUserById(Long id) {
         return userRepository.findByID(id);
