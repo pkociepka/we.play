d219ac46e3d88c21f9946346cb184bef8682a2f9
Kurtzz <przemyslaw.kurc@gmail.com>
2016-10-18 12:20:33 +0200

    Fix AuthorityRepository

--- src/main/java/pl/edu/agh/weplay/repository/AuthorityRepository.java
+++ src/main/java/pl/edu/agh/weplay/repository/AuthorityRepository.java
@@ -6,5 +6,5 @@ import pl.edu.agh.weplay.domain.Authority;
 /**
  * Created by P on 18.10.2016.
  */
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
 }
