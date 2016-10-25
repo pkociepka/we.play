1895b4a6bbc6fd99fd3528c36e2d48c75146e29f
Kurtzz <przemyslaw.kurc@gmail.com>
2016-10-25 17:58:13 +0200

    Add user and account service

---
+++ src/main/resources/static/js/services/auth/account.service.js
@@ -0,0 +1,27 @@
+/**
+ * Created by P on 25.10.2016.
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('weplay')
+        .factory('Account', Account);
+
+    Account.$inject = ['$resource'];
+
+    function Account ($resource) {
+        var service = $resource('api/account', {}, {
+            'get': { method: 'GET', params: {}, isArray: false,
+                interceptor: {
+                    response: function(response) {
+                        // expose response
+                        return response;
+                    }
+                }
+            }
+        });
+
+        return service;
+    }
+})();
---
+++ src/main/resources/static/js/services/user.service.js
@@ -0,0 +1,30 @@
+/**
+ * Created by P on 25.10.2016.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('weplay')
+        .factory('User', User);
+
+    User.$inject = ['$resource'];
+
+    function User ($resource) {
+        var service = $resource('api/users/:login', {}, {
+            'query': {method: 'GET', isArray: true},
+            'get': {
+                method: 'GET',
+                transformResponse: function (data) {
+                    data = angular.fromJson(data);
+                    return data;
+                }
+            },
+            'save': { method:'POST' },
+            'update': { method:'PUT' },
+            'delete':{ method:'DELETE'}
+        });
+
+        return service;
+    }
+})();
