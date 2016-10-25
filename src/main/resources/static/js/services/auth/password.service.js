08c83d455b9edb6c622f0219c321bc0621d32cf4
Kurtzz <przemyslaw.kurc@gmail.com>
2016-10-25 17:55:15 +0200

    Add password service

---
+++ src/main/resources/static/js/services/auth/password.service.js
@@ -0,0 +1,18 @@
+/**
+ * Created by P on 25.10.2016.
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('weplay')
+        .factory('Password', Password);
+
+    Password.$inject = ['$resource'];
+
+    function Password($resource) {
+        var service = $resource('api/account/change_password', {}, {});
+
+        return service;
+    }
+})();
