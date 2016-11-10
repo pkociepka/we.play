d505276ad58ed5aa65e642b0ff2c921a7190a1bd
Kurtzz <przemyslaw.kurc@gmail.com>
2016-11-10 14:58:23 +0100

    Add login function

--- src/main/resources/static/js/services/auth/auth.service.js
+++ src/main/resources/static/js/services/auth/auth.service.js
@@ -11,9 +11,10 @@
         console.log("Auth service");
 
         var service =  {
+            authorize: authorize,
             createAccount: createAccount,
-            logout: logout,
-            authorize: authorize
+            login: login,
+            logout: logout
         };
 
         return service;
@@ -60,6 +61,28 @@
                     this.logout();
                     return cb(err);
                 }.bind(this)).$promise;
+        }
+
+        function login (credentials, callback) {
+            var cb = callback || angular.noop;
+            var deferred = $q.defer();
+
+            AuthServerProvider.login(credentials)
+                .then(loginThen)
+                .catch(function (err) {
+                    this.logout();
+                    deferred.reject(err);
+                    return cb(err);
+                }.bind(this));
+
+            function loginThen (data) {
+                Principal.identity(true).then(function(account) {
+                    deferred.resolve(data);
+                });
+                return cb();
+            }
+
+            return deferred.promise;
         }
 
         function logout () {
