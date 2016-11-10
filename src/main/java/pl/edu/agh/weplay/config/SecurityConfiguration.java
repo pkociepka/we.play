7b35a3a025c400af6cf282b2108d425a49ea7961
Kurtzz <przemyslaw.kurc@gmail.com>
2016-11-10 15:36:54 +0100

    Fix security config

--- src/main/java/pl/edu/agh/weplay/config/SecurityConfiguration.java
+++ src/main/java/pl/edu/agh/weplay/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,7 +21,7 @@ import pl.edu.agh.weplay.security.AjaxLogoutSuccessHandler;
 import javax.inject.Inject;
 
 @Configuration
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -43,17 +44,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-                .antMatchers(HttpMethod.OPTIONS, "/**");
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .antMatchers("/js/**", "/node_modules/**", "/css/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().and().csrf().disable()
-//            .and()
                 .authorizeRequests()
                 .antMatchers("/index.html", "/home.html", "/login.html", "/", "/user").permitAll()
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
 //            .and()
 //                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 //            .and()
