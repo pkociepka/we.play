5febfbe0370b7df7c732d8343e6fe20e5083d88b
Kurtzz <przemyslaw.kurc@gmail.com>
2016-11-10 16:00:19 +0100

    Add nice sign in dialog

--- src/main/resources/static/js/navbar/navbar.controller.js
+++ src/main/resources/static/js/navbar/navbar.controller.js
@@ -5,11 +5,12 @@
         .module('weplay')
         .controller('navigation', navigation);
 
-    navigation.$inject = ['$route'];
+    navigation.$inject = ['$route', 'LoginService'];
 
-    function navigation($route) {
+    function navigation($route, LoginService) {
         var vm = this;
         vm.tab = tab;
+        vm.login = LoginService.open;
 
         function tab(route) {
             return $route.current && route === $route.current.controller;
