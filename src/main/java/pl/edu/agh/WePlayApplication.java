0920a82bc4cbebb14cf0e3258775e05b9fd7a87a
Kurtzz <przemyslaw.kurc@gmail.com>
2016-11-13 21:14:45 +0100

    Enable configuration properties

--- src/main/java/pl/edu/agh/WePlayApplication.java
+++ src/main/java/pl/edu/agh/WePlayApplication.java
@@ -2,8 +2,11 @@ package pl.edu.agh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.edu.agh.weplay.config.Properties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({Properties.class})
 public class WePlayApplication {
 
 	public static void main(String[] args) {
