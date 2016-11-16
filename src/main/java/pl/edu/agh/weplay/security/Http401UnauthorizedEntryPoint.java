003a5e1e560ce9181b9f84fcd979f67b7e77529c
Kurtzz <przemyslaw.kurc@gmail.com>
2016-11-16 22:39:57 +0100

    Add http 401 unauthorized entry point

---
+++ src/main/java/pl/edu/agh/weplay/security/Http401UnauthorizedEntryPoint.java
@@ -0,0 +1,30 @@
+package pl.edu.agh.weplay.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by P on 16.11.2016.
+ */
+@Component
+public class Http401UnauthorizedEntryPoint implements AuthenticationEntryPoint {
+
+    /**
+     * Always returns a 401 error code to the client.
+     */
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2)
+            throws IOException,
+            ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied");
+    }
+}
