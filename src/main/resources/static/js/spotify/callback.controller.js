2bfc6072e60fa1b78cc6693167c54fa347460470
Kurtzz <przemyslaw.kurc@gmail.com>
2016-12-15 18:18:32 +0100

    Add http post request to persist spotify token

--- src/main/resources/static/js/spotify/callback.controller.js
+++ src/main/resources/static/js/spotify/callback.controller.js
@@ -9,12 +9,18 @@
         .controller('CallbackController', CallbackController);
 
 
-    CallbackController.$inject = ['$location'];
+    CallbackController.$inject = ['$location', '$http', '$window'];
 
-    function CallbackController($location) {
+    function CallbackController($location, $http, $window) {
         var vm = this;
         vm.access_token = $location.search().access_token;
         vm.token_type = $location.search().token_type;
         vm.expires_in = $location.search().expires_in;
+
+        return $http.post('/api/spotifytoken/' + vm.access_token + '/' + vm.expires_in).success(function (response) {
+            $window.close();
+            $window.alert('Success!');
+            return response;
+        });
     }
 })();
