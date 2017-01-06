acc412bf0756dcc4e3212e3160848f3479e291bc
Kurtzz <przemyslaw.kurc@gmail.com>
2017-01-06 14:11:54 +0100

    Add login to POST request

--- src/main/resources/static/js/spotify/callback.controller.js
+++ src/main/resources/static/js/spotify/callback.controller.js
@@ -9,19 +9,29 @@
         .controller('CallbackController', CallbackController);
 
 
-    CallbackController.$inject = ['$location', '$http', '$window'];
+    CallbackController.$inject = ['$location', '$http', '$window', 'Principal'];
 
-    function CallbackController($location, $http, $window) {
+    function CallbackController($location, $http, $window, Principal) {
         var vm = this;
+        vm.account = null;
         vm.access_token = $location.search().access_token;
         vm.token_type = $location.search().token_type;
         vm.expires_in = $location.search().expires_in;
 
-        return $http.post('http://localhost:5000/api/spotify/sync/' + vm.access_token + '/' + vm.expires_in)
-            .then(function (response) {
-                $window.close();
-                $window.alert('Success!');
-                return response.data;
-            });
+        Principal.identity().then(function(account) {
+            vm.account = account;
+            console.log(vm.account.login);
+        }).then(function () {
+            return $http.post('http://localhost:5000/api/spotify/sync/' + vm.access_token + '/' + vm.expires_in + '/' + vm.account.login)
+                .then(function (response) {
+                    $window.close();
+                    $window.alert('Success!');
+                    return response.data;
+                }, function (response) {
+                    $window.close();
+                    $window.alert('Failure!');
+                    return response.data;
+                });
+        });
     }
 })();
